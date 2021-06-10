5f19199c79aeabdc725ce4cf5c0545032f3f2793
Nicolas <nicolas_31_07@hotmail.com>
2021-06-09 21:48:20 -0300

    update hero

--- potfolio-react/src/components/Views/Hero/Hero.js
+++ potfolio-react/src/components/Views/Hero/Hero.js
@@ -1,26 +1,24 @@
 import React from "react";
 import "./styles-hero.css";
 import imgHero from "./img/hero2.svg";
-import imgWave from "./img/wave1.svg";
 
 export const Hero = () => {
   return (
     <div>
       <div className="container-hero" id="hero">
-
         <div className="hero__izquierda">
           <h2 className="hero-title">Bienvenidos!</h2>
           <p className="hero-text">
-            Me llamo <b className="orange">Nico,</b> soy desarrollador <b className="orange">Front End</b> 
-            <p>y este es mi <b className="orange">Portafolio. </b>Espero que lo disfrutes!</p>
-           
+            Me llamo <b className="orange">Nicolas Ramirez,</b> soy
+            desarrollador <b className="orange">Front End</b> y este es mi{" "}
+            <b className="orange">Portafolio. </b>Espero que lo disfrutes!
           </p>
+        {/* <button className="button-cv">Download CV</button> */}
         </div>
         <div className="hero__derecha">
           <img src={imgHero} alt="" />
         </div>
       </div>
-      {/* <img className="wave" src={imgWave} alt="" /> */}
     </div>
   );
 };
--- potfolio-react/src/components/Views/Proyectos/Proyecto.js
+++ potfolio-react/src/components/Views/Proyectos/Proyecto.js
@@ -12,6 +12,6 @@ const Proyecto = (props) => {
       </div>
     </div>
   );
-};
+};  
 
 export default Proyecto;
